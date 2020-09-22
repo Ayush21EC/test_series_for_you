30aef0b01e6b565d2527fb1352aaf1e9fbb6b312
Ayush21EC <61827144+Ayush21EC@users.noreply.github.com>
2020-09-22 09:37:22 +0530

    Add files via upload

---
+++ src/online status/online-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-online-status',
+  templateUrl: './online-status.component.html',
+  styleUrls: ['./online-status.component.css']
+})
+export class OnlineStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
